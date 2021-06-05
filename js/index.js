60261547d23a969e5d279701c9c207a18815c3d4
Lisa Ostman <peolina@live.com.au>
2021-06-05 13:54:24 +1000

    learning intersection observors to fade in project sections

---
+++ js/index.js
@@ -0,0 +1,47 @@
+const fadingElements = document.querySelectorAll('.fade-in');
+const slidingElements = document.querySelectorAll('.slide-in');
+
+const options = {
+    threshold: 0,
+    rootMargin: '0px 0px -250px 0px'
+}
+
+const appearOnScroll = new IntersectionObserver(
+    function (
+        entries,
+        appearOnScroll
+    ) {
+        entries.forEach(entry => {
+            if (!entry.isIntersecting) {
+                return;
+            }
+            else {
+                console.log(entry.target);
+                console.log("hello!");
+                entry.target.classList.add('appear');
+                appearOnScroll.unobserve(entry.target);
+            }
+        })
+    }, 
+options)
+
+fadingElements.forEach(fading => {
+    appearOnScroll.observe(fading);
+})
+
+slidingElements.forEach(slider => {
+    appearOnScroll.observe(slider);
+})
+
+// const checkpoint = 1000;
+ 
+// window.addEventListener("scroll", () => {
+//   const currentScroll = window.pageYOffset;
+//   let opacity;
+//   if (currentScroll <= checkpoint) {
+//     opacity = 0;
+//   } else {
+//     opacity = currentScroll / checkpoint - 0.5;
+//   }
+//   document.querySelector(".firstSection").style.opacity = opacity;
+// });
